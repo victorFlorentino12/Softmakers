2ec5355deb66148dc02678cdce704ebe44b0e451
victorFlorentino12 <140027327+victorFlorentino12@users.noreply.github.com>
2024-01-06 10:46:51 -0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import { useAuth } from '../../context/AuthProvider/useAuth';
-import styles from './Home.module.css';
-import { useNavigate } from 'react-router-dom';
 import SideMenu from '../../components/SideMenu/SideMenu';
 import Header from '../../components/Header/Header';
 
 
 const Home = () => {
-  const auth = useAuth();
-  const navigate = useNavigate()
-  const handleLogout = async () => {
-    try {
-      await auth.logout(); // Invoca a função logout corretamente
-      navigate('/')
-    } catch (error) {
-      console.error('Erro ao fazer logout:', error);
-    }
-  };
+
 
   return (
     <div>
-    
+      <Header/>
       <SideMenu/>
       
     </div>
