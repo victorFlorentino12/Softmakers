9ae171f2b5bbc7f6b870f9f45b8d3be644d8f7ca
AmandaSoares <amandags_soares@hotmail.com>
2024-01-06 00:04:08 -0300

    Add header properties

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 import './App.css'
 import Header from './components/Header/Header'
+import UserMenu from './components/Header/UserMenu';
 import SideMenu from './components/SideMenu/SideMenu'
 
 function App() {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,51 @@
+import React, { useState, useEffect } from 'react';
 import styles from './Header.module.css'
+import { FaBell, FaUserCircle} from "react-icons/fa"
+import Logo from "./Logo"; 
+import UserMenu from './UserMenu';
+
+
 
 const Header = () => {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://reqres.in/api/users/2');
+        const data = await response.json();
+
+        if (data.data && data.data.first_name) {
+          setFirstName(data.data.first_name);
+        }
+        //Aqui tem que alterar pro cargo, coloquei só pra testar
+        if (data.data && data.data.last_name) {
+          setLastName(data.data.last_name);
+        }
+
+      } catch (error) {
+        console.error('Erro ao buscar dados da API', error);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
   return (
-    <div>Header</div>
+    <header>
+      <div className={styles.header_logo}>
+        <Logo/>
+      </div>
+      <div className={styles.header_login}>
+          <UserMenu />
+          <div className={styles.header_user_dates}>
+            <p>{`${firstName} ${lastName}`}</p>
+            <p>Cargo</p>
+          </div>
+        </div>
+    </header>
   )
 }
 
